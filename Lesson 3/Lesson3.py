b00abdae059e3d0fa46c607a90396916a9adf591
EG-QA-12 <egskypro@mail.ru>
2023-03-20 12:46:02 +0300

    udate

--- Lesson 3/Lesson3.py
+++ Lesson 3/Lesson3.py
@@ -1,11 +1,11 @@
 from user import User
 from card import Card
 
-alex - User("Alex")
+alex = User("Alex")
 
 
 alex.sayName()
-alex.satAge(33)
+alex.setAge(33)
 alex.sayAge()
 
 card = Card("4353 1234 5678 8765", "11/28", "Alex F")
