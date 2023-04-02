9c06f324bbf4289929f81206c496652575b94c07
EG-QA-12 <egskypro@mail.ru>
2023-04-02 23:21:17 +0300

    Калькулятор (тест)

---
+++ Lesson 4/calculator.py
@@ -0,0 +1,29 @@
+class Calculator:
+
+    def sum(self, a, b):
+        return a+b
+
+    def div(self, a, b):
+        if (b == 0):
+            raise ArithmeticError("нельзя делить на ноль") 
+        else:
+            return a/b;
+
+    def mul(self, a, b):
+        return a*b
+    
+    def sub(self, a, b):
+        return a-b
+    
+    def avg(self, arr):
+        len_of_arr = len(arr)
+        if len_of_arr == 0:
+            return 0
+     
+        total = 0
+        for x in arr:
+            if not isinstance(x, (int, float)):
+             raise TypeError("элементы списка должны быть числами")
+        total += x
+        avg = self.div(total, len_of_arr) 
+        return avg
