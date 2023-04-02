df89aa2f797f39ed074efb7cc997ad623419632c
EG-QA-12 <egskypro@mail.ru>
2023-04-02 20:26:22 +0300

    Нарисуйте картинку (task4)

--- Lesson 3/Lesson_3 _task_4.py
+++ Lesson 3/Lesson_3 _task_4.py
@@ -1,68 +1,103 @@
 from turtle import *
 
-# создаем объект черепашки
 my_turtle = Turtle()
-
-# настраиваем скорость рисования и размер экрана
 my_turtle.speed(0)
-my_turtle.screen.setup(800, 600)
+my_turtle.screen.setup(1200, 800)
 
-# задаем цвет заливки и обводки для черепашки
-my_turtle.color("brown", "white")
-
-# рисуем голову кролика
+# Нарисовать тело
+my_turtle.penup()
+my_turtle.goto(-80,0)
+my_turtle.pendown()
 my_turtle.begin_fill()
-my_turtle.circle(50)
+my_turtle.fillcolor("#F5A9D0")
+my_turtle.circle(120)
 my_turtle.end_fill()
 
-# рисуем тело кролика
-my_turtle.right(90)
-my_turtle.forward(100)
+# Нарисовать левое ухо
+my_turtle.penup()
+my_turtle.goto(-140,220)
+my_turtle.pendown()
+my_turtle.begin_fill()
+my_turtle.fillcolor("#F5A9D0")
 my_turtle.right(45)
-my_turtle.begin_fill()
-my_turtle.circle(50, 180)
-my_turtle.right(90)
-my_turtle.circle(50, 180)
+my_turtle.circle(100,80)
+my_turtle.circle(25,130)
+my_turtle.circle(100,80)
 my_turtle.end_fill()
-my_turtle.right(135)
-my_turtle.forward(100)
 
-# рисуем уши кролика
-my_turtle.right(135)
-my_turtle.forward(50)
-my_turtle.right(90)
-my_turtle.begin_fill()
-my_turtle.circle(30, 180)
-my_turtle.right(90)
-my_turtle.circle(30, 180)
-my_turtle.end_fill()
-my_turtle.right(90)
-my_turtle.forward(50)
-
-# рисуем глаза кролика
+# Нарисовать правое ухо
 my_turtle.penup()
-my_turtle.goto(-15, 20)
+my_turtle.goto(60,220)
 my_turtle.pendown()
-my_turtle.color("black")
 my_turtle.begin_fill()
+my_turtle.fillcolor("#F5A9D0")
+my_turtle.left(45)
+my_turtle.circle(-100,80)
+my_turtle.circle(-25,130)
+my_turtle.circle(-100,80)
+my_turtle.end_fill()
+
+# Нарисовать левый глаз
+my_turtle.penup()
+my_turtle.goto(-40,80)
+my_turtle.pendown()
+my_turtle.begin_fill()
+my_turtle.fillcolor("white")
+my_turtle.circle(30)
+my_turtle.end_fill()
+
+# Нарисовать правый глаз
+my_turtle.penup()
+my_turtle.goto(50,80)
+my_turtle.pendown()
+my_turtle.begin_fill()
+my_turtle.fillcolor("white")
+my_turtle.circle(30)
+my_turtle.end_fill()
+
+# Нарисовать зрачки
+my_turtle.penup()
+my_turtle.goto(-25,70)
+my_turtle.pendown()
+my_turtle.begin_fill()
+my_turtle.fillcolor("black")
 my_turtle.circle(10)
 my_turtle.end_fill()
+
 my_turtle.penup()
-my_turtle.goto(15, 20)
+my_turtle.goto(65,70)
 my_turtle.pendown()
 my_turtle.begin_fill()
+my_turtle.fillcolor("black")
 my_turtle.circle(10)
 my_turtle.end_fill()
 
-# рисуем нос кролика
+# Нарисовать нос
 my_turtle.penup()
-my_turtle.goto(0, 0)
+my_turtle.goto(0,0)
 my_turtle.pendown()
 my_turtle.begin_fill()
-my_turtle.circle(5)
+my_turtle.fillcolor("#F7BE81")
+my_turtle.circle(20)
 my_turtle.end_fill()
 
-# завершаем работу
-my_turtle.screen.exitonclick()
-my_turtle.screen.mainloop()
+# Нарисовать рот
+my_turtle.penup()
+my_turtle.goto(-70,-50)
+my_turtle.pendown()
+my_turtle.right(90)
+my_turtle.circle(70, 180)
 
+# Нарисовать лапы
+my_turtle.penup()
+my_turtle.goto(-70,-120)
+my_turtle.pendown()
+my_turtle.right(30)
+my_turtle.begin_fill()
+my_turtle.fillcolor("#F5A9D0")
+my_turtle.circle(70, 240)
+my_turtle.right(150)
+my_turtle.forward(140)
+my_turtle.right(150)
+my_turtle.circle(70, 240)
+my_turtle
