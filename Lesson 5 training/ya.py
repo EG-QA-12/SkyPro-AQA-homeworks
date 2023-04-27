434fa08e98427bc3562a45b2bd5df6162ff0b9c0
EG-QA-12 <egskypro@mail.ru>
2023-04-28 00:25:10 +0300

    wendriver training

---
+++ Lesson 5 training/ya.py
@@ -0,0 +1,19 @@
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromiumService
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
+
+driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+
+driver.get("https://ya.ru")
+
+driver.get("https://vk.com")
+
+for x in range (1, 10):
+    driver.back()
+    driver.forward()
+    driver.refresh()
+
+sleep(50)
