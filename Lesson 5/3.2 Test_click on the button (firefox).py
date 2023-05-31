cf2b04aba744c69ebaaa2bad6e0819420f31d00d
EG-QA-12 <egskypro@mail.ru>
2023-05-31 23:09:59 +0300

    Задание 3

--- Lesson 5/3.2 Test_click on the button (firefox).py
+++ Lesson 5/3.2 Test_click on the button (firefox).py
@@ -1,14 +1,15 @@
-from time import sleep
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromiumService
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+# Устанавливаем путь к драйверу Firefox
+driver_service = FirefoxService(executable_path=GeckoDriverManager().install())
+
+# Инициализируем драйвер Firefox
+driver = webdriver.Firefox(service=driver_service)
 driver.maximize_window()
 driver.get("http://the-internet.herokuapp.com/add_remove_elements/")
 
@@ -16,18 +17,16 @@ driver.get("http://the-internet.herokuapp.com/add_remove_elements/")
 add_button = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.XPATH, "//button[@onclick='addElement()']"))
 )
+
 # Кликаем на кнопку Add Element пять раз
 for _ in range(5):
     add_button.click()
-sleep(5) 
+
 # Находим все кнопки Delete
 delete_buttons = driver.find_elements(By.XPATH, "//button[@class='added-manually']")
 
 # Выводим размер списка кнопок Delete
 print("Размер списка кнопок Delete:", len(delete_buttons))
 
-
-# Закрываем веб-драйвер
+# Закрываем браузер
 driver.quit()
-sleep(1)
-
