5e75bda3ce3fc6ce792eb636cb9e1997f6d88960
EG-QA-12 <egskypro@mail.ru>
2023-06-01 23:27:42 +0300

    Задание 5

--- Lesson 5 training/ya.py
+++ Lesson 5 training/ya.py
@@ -1,19 +1,14 @@
 from time import sleep
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromiumService
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
 
-driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
-driver.maximize_window()
-driver.get("https://ya.ru")
+browser = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+browser.maximize_window()
+browser.get("https://ya.ru")
+sleep(5)
 
-driver.get("https://vk.com")
+browser.save_screenshot('./ya.png')
+browser.quit()
 
-for x in range (1, 10):
-    driver.back()
-    driver.forward()
-    driver.refresh()
-
-sleep(15)
