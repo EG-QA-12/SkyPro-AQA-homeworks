8e242c4e6c450860d18093c9fb8d048569c8c304
EG-QA-12 <egskypro@mail.ru>
2023-07-16 18:47:23 +0300

    Lesson 6 Tests

---
+++ Обучение/1.py
@@ -0,0 +1,25 @@
+# This code sample uses the 'requests' library:
+# http://docs.python-requests.org
+import requests
+import json
+
+url = "https://api.trello.com/1/boards/{id}/memberships"
+
+headers = {
+  "Accept": "application/json"
+}
+
+query = {
+  'key': 'APIKey',
+  'token': 'APIToken'
+}
+
+response = requests.request(
+   "GET",
+   url,
+   headers=headers,
+   params=query
+)
+
+print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
+
