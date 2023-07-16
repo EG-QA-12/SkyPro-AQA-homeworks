e4de8c4e1213bce12e24f25caedc32ebac27696e
EG-QA-12 <egskypro@mail.ru>
2023-07-16 18:47:44 +0300

    Lesson 6 Tests

---
+++ Lesson 6/Test 2   Rename the button.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromiumService
+from webdriver_manager.chrome import ChromeDriverManager, ChromeType
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Создаем экземпляр драйвера
+driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+driver.maximize_window()
+
+# Шаг 1: Переходим на сайт
+driver.get("http://uitestingplayground.com/textinput")
+
+# Шаг 2: Вводим текст "SkyPro" в поле ввода
+
+#input_field = driver.find_element(By.CSS_SELECTOR, "input.form-control[type='text'][placeholder='MyButton'][id='newButtonName']")
+input_field = driver.find_element(By.ID, "newButtonName")
+input_field.send_keys("SkyPro")
+
+# Шаг 3: Нажимаем на синюю кнопку
+blue_button = driver.find_element(By.CSS_SELECTOR, "button.btn-primary")
+blue_button.click()
+
+# Шаг 4: Получаем текст кнопки и выводим в консоль
+button_text = blue_button.text
+print(button_text)
+
+# Закрываем браузер
+driver.quit()
+
+
+
+
+
+
