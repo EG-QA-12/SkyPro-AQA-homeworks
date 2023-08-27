3ddf397c12e5b65dc9ebd9af37260aefad92107e
EG-QA-12 <egskypro@mail.ru>
2023-08-28 00:44:00 +0300

    Lesson 7_Task 1

--- Lesson 7 training/Pages/labirint_page_object_test.py
+++ Lesson 7 training/Pages/labirint_page_object_test.py
@@ -1,9 +1,10 @@
 from time import sleep
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service as GeckoService
-from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.service import Service as ChromiumService
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -12,7 +13,7 @@ from Pages.ResultPage import ResultPage
 from Pages.CartPage import CartPage
 
 def test_cart_counter():
-    browser = webdriver.Firefox(service=GeckoService(executable_path=GeckoDriverManager().install()))
+    browser = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
 
     main_page = MainPage(browser)
     main_page.set_cookie_policy()
@@ -25,21 +26,20 @@ def test_cart_counter():
     cart_page = CartPage(browser)
     cart_page.get()
     as_is = cart_page.get_counter()
-    to_be = 10  # Определите ожидаемое значение для to_be
 
     assert as_is == to_be
 
     browser.quit()
 
 def test_empty_search_result():
-    browser = webdriver.Firefox(service=GeckoService(executable_path=GeckoDriverManager().install()))
-
+    browser = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+ 
     main_page = MainPage(browser)
     main_page.set_cookie_policy()
     main_page.search("no book search term")
 
     result_page = ResultPage(browser)
     msg = result_page.get_empty_result_message()
-
+    
     assert msg == 'Мы ничего не нашли по вашему запросу! Что делать?'
     browser.quit()
