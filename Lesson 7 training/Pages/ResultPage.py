4bca03bcc7b48338a86e62c98905b7c65f7a5f22
EG-QA-12 <egskypro@mail.ru>
2023-08-27 23:32:57 +0300

    Lesson7 training

--- Lesson 7 training/Pages/ResultPage.py
+++ Lesson 7 training/Pages/ResultPage.py
@@ -4,22 +4,21 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class ResultPage:
     def __init__(self, browser):
-    self._driver = browser
+        self._driver = browser
 
     def switch_to_table(self):
-    sel_driver.find_element(By.CSS_SELECTOR, 'a[title="таблицей"]').click()
-    WebDriverWait( sel_driver, 10).until(
-        EC.presence_of_all_elements_located((By.CSS_SELECTOR, "table"))
+        self._driver.find_element(By.CSS_SELECTOR, 'a[title="таблицей"]').click()
+        WebDriverWait(self._driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "table")))
 
-def add_books (self)
-    buy_buttons = browser.find_elements(By.CSS_SELECTOR, 'btn.buy-link.btn-primary')
-    counter = 0
-    for btn in buy_buttons:
-        btn.click()
-        counter += 1
-  
-    return counter
-def get_empty_result_message (self):
-div = self.driver.find_element (By.CSS_SELECTOR, "div.search-error")
-h1 = div.find_element (By.CSS_SELECTOR, "h1")
-return h1.text
+    def add_books(self):
+        buy_buttons = self._driver.find_elements(By.CSS_SELECTOR, 'btn.buy-link.btn-primary')
+        counter = 0
+        for btn in buy_buttons:
+            btn.click()
+            counter += 1
+        return counter
+
+    def get_empty_result_message(self):
+        div = self._driver.find_element(By.CSS_SELECTOR, "div.search-error")
+        h1 = div.find_element(By.CSS_SELECTOR, "h1")
+        return h1.text
--- Pages/CartPage.py
+++
@@ -1,8 +0,0 @@
-class CartPage:
-
-    def __init__(self, browser):
-        self._driver = browser
-
-        def get():
-
-            self._driver.get("https://www.labirint.ru/cart")
