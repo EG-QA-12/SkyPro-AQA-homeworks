c1ad3cc1bb7d6d0b94af4e7bf54f90c5a0dbc4a4
EG-QA-12 <egskypro@mail.ru>
2023-10-24 17:45:36 +0300

    Lesson 7

--- Lesson 7/pages/calculator_page.py
+++ Lesson 7/pages/calculator_page.py
@@ -1,41 +1,31 @@
+# calculator_page.py
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-class CalculatorPage:
-    URL = "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
 
+class CalculatorPage:
     def __init__(self, driver):
         self.driver = driver
 
-    def open(self):
-        self.driver.get(self.URL)
+    def open_page(self, url):
+        # Открываем указанную страницу
+        self.driver.get(url)
 
-    def enter_value(self, value):
-        delay_input = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "#delay"))
-        )
+    def enter_delay_value(self, delay_value):
+        # Находим поле ввода и вводим в него заданное значение
+        delay_input = self.driver.find_element(By.CSS_SELECTOR, "#delay")
         delay_input.clear()
-        delay_input.send_keys(value)
+        delay_input.send_keys(delay_value)
 
-    def click_button_7(self):
-        button_7 = self.driver.find_element(By.XPATH, "//span[contains(@class, 'btn-outline-primary') and text()='7']")
-        button_7.click()
+    def click_button(self, button_locator):
+        # Находим кнопку по указанному локатору и кликаем на нее
+        button = self.driver.find_element(By.CSS_SELECTOR, button_locator)
+        button.click()
 
-    def click_button_plus(self):
-        button_plus = self.driver.find_element(By.XPATH, "//span[contains(@class, 'btn-outline-success') and text()='+']")
-        button_plus.click()
-
-    def click_button_8(self):
-        button_8 = self.driver.find_element(By.XPATH, "//span[contains(@class, 'btn-outline-primary') and text()='8']")
-        button_8.click()
-
-    def click_button_equal(self):
-        button_equal = self.driver.find_element(By.XPATH, "//span[contains(@class, 'btn-outline-warning') and text()='=']")
-        button_equal.click()
-
-    def get_result(self):
-        result_element = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.ID, "result"))
+    def get_result_text(self):
+        # Ожидаем появление элемента "screen" и возвращаем его текст
+        result_element = WebDriverWait(self.driver, 45).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "div.screen"))
         )
-        return
+        return result_element.text
