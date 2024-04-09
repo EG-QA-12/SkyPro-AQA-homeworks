01f89a5368976d3874f084950ed5fde7e967a26a
EG-QA-12 <egskypro@mail.ru>
2024-04-09 22:43:25 +0300

    update lessons

--- Lesson 6/Test 1 Pressing the button.py
+++ Lesson 6/Test 1 Pressing the button.py
@@ -1,12 +1,11 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromiumService
-from webdriver_manager.chrome import ChromeDriverManager, ChromeType
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
-driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.maximize_window()
 
 driver.get("http://uitestingplayground.com/ajax")
@@ -22,7 +21,3 @@ text = green_banner.text
 print(text)
 
 driver.quit()
-
-
-
-
