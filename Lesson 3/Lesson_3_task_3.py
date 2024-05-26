8f9871b434ca3dafa21db83fb09f6cfd53554d34
EG-QA-12 <egskypro@mail.ru>
2024-05-26 11:05:55 +0300

    update lesson 3

--- Lesson 3/Lesson_3_task_3.py
+++ Lesson 3/Lesson_3_task_3.py
@@ -1,12 +1,18 @@
+"""
+Модуль для работы с почтовыми отправлениями.
+"""
+
 from Address import Address
 from Mailing import Mailing
 
-# создаем два адреса
+# Создаем два адреса
 to_address = Address("123456", "Москва", "Красная площадь", "1", "2")
 from_address = Address("654321", "Санкт-Петербург", "Невский проспект", "3", "4")
 
-# создаем почтовое отправление
+# Создаем почтовое отправление
 mailing = Mailing(to_address, from_address, 1000, "ABC123")
 
-# печатаем информацию о почтовом отправлении
-print(f"Отправление {mailing.track} из {mailing.from_.city}, {mailing.from_.street}, {mailing.from_.house}-{mailing.from_.apartment} в {mailing.to.city}, {mailing.to.street}, {mailing.to.house}-{mailing.to.apartment}. Стоимость {mailing.cost} рублей.")
+# Печатаем информацию о почтовом отправлении
+print(f"Отправление {mailing.track} из {mailing.from_.city}, {mailing.from_.street},"
+      f" {mailing.from_.house}-{mailing.from_.apartment} в {mailing.to.city}, {mailing.to.street},"
+      f"{mailing.to.house}-{mailing.to.apartment}. Стоимость {mailing.cost} рублей.")
